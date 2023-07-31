d116bcb53af10f6801ba22e3d75d8bed6363dfe8
JohnWicc69420 <tusmantariq9111@gmail.com>
2023-07-31 22:18:02 +0500

    same

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,20 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
   params: {
+    q: 'desp',
     hl: 'en',
     gl: 'US'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY || '1c67d148e5mshf2917eb350b1d98p1e1b7fjsn285938f4a483',
     'X-RapidAPI-Host': 'youtube138.p.rapidapi.com',
   },
 };
 
 export const fetchDataFromApi = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options)
+  return data;
 }
